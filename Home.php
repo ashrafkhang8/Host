7ed9050c5ede0cc3ff2bacef88dbb7e2a2e29b3e
ashrafkhang8 <83918822+ashrafkhang8@users.noreply.github.com>
2021-05-19 22:06:43 +0530

    Add files via upload

---
+++ Home.php
@@ -0,0 +1,167 @@
+
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <meta name="robots" content="noindex, nofollow">
+      <title>Users</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        
+          <link rel="stylesheet" href="style2.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+      
+      <style type="text/css">
+
+         .container  {
+			 margin-top: 10px;
+			 border: px outset #9C9C9C;
+			 background-color: #fff;
+			 padding:30px;
+         }    
+		 .container h2{
+			margin-bottom:25px;
+		 }
+		 .text-info {
+			color: #000 !important;
+		}
+		
+		.btn {
+		    color:Green;
+		}
+		
+footer{
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    border-top: 1px solid rgba(0,0,0,0.1);
+ 
+}
+footer p{
+    color:#5e5e5e;
+    margin: 10px 0px;
+    font-size: 0.9rem;
+}
+
+		
+      </style>
+      
+      
+      <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="index.css">
+        <link rel="stylesheet" href="style1.css">
+ </head>
+   
+     <body>
+       
+           <nav>
+      <input type="checkbox" id="check">
+      <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+      </label>
+      <label class="logo">Free-Hosting-X™</label>
+      <ul>
+        <li><a class="active" href="https://99473ef82b9a.ngrok.io">phpMyAdmin</a></li>
+        <li><a href="Admin/index.php">F-Panel</a></li>
+        <li><a href="register/register.php">Register</a></li>
+      </ul>
+ </nav>
+       
+ 
+       
+       
+    <div class="container">
+ 
+         <table class="table table-striped 
+         table-bordered">
+            <thead>
+               <tr>
+                  <th width="20%">phpMyAdmin</th>
+                  <th width="20%">F-panel</th>
+                  <th width="15%">Register</th>
+                   <th width="15%">Status</th>
+               </tr>
+            </thead>
+ 
+           <tbody id="user_grid">
+			    <tr>
+           <td>
+             <button type="button" class="btn">
+               <a href="https://99473ef82b9a.ngrok.io">Login</a>
+             </button>
+           </td>
+             
+<td>
+<button type="button" class="btn">
+<a href="Admin/index.php">Login</a>
+</button>
+</td>
+
+    <td><button type="button" class="btn"><a href="register/register.php">Signup</a>
+    </button></td>
+    
+      <td><button type="button" class="btn"><a href="register/register.php">Login</a>
+    </button></td>
+    
+       
+          </tr>
+           </tbody>
+    <div class="container">
+      
+         <h2 class="text-center text-info">Best Website Hosting</h2>
+         
+         <table class="table table-striped 
+         table-bordered">
+            <thead>
+                  <p align="center"><b>
+                      <a href="register/register.php">Register Account<Br> </a> <B>Important Note !👇 </B> <Br> ,Your account will be activated in 24 hours</b></p>
+                     <P Align="center"><i>
+                       <a Href="https://t.me/hostingwebs">Telegram Join !</a> Any Problem & Issues send Error Massage On Telegram </i></P>
+
+            </thead>
+            
+
+
+            </tbody>
+      
+         </table>
+         
+             
+   <img  src="php.png" width="100vh">
+   <P Align=""><B>Unlimited Create Database</B></P>
+   <Br>
+            
+  <img align="" src="St.png" width="100vh">
+     <P Align=""><B>Limited Storage 1gb</B></P>
+     
+     <Br>
+         
+           <img align="" src="cloud.png" width="100vh">
+     <P Align=""><B><a href="">Secure Cloud Storage</a></B></P>
+         
+         
+     <Br>    
+     
+       <P Align="center"><B>Simply fast websites</B><Br>
+<I>Website speed can slow or grow your business. Delight your visitors with a lightning fast website</I>.</P>
+         
+         
+        <P Align="Center"><B><A Href="/wordpress-5.7.1.zip" Download>WordPress-5.7.1 made easy Download Zip </A></B><Br>
+<I>Start your website with an automatic 1-click WordPress installation. The backend is powered by LiteSpeed caching and advanced optimization to ensure your websites are fast, reliable and secure.</I></P>
+         
+         
+      
+         </div>
+    <!--footer------------------>
+    <footer>
+        <p> Free Web HOSTING | Hosting-X™
+       <Br> ©2004-2021 Design BY ASHRAF- Cheap web hosting  provider.
+
+    </footer>
+    
+   </body>
+</html>
+
+
+  
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Logo</title>
+  <link type="text/css" rel="stylesheet" href="style.css" />
+  <head>
+  <meta http-equiv="refresh" content="3; url=Home.php" >
+</head>
+  
+</head>
+
+<body>
+
+  <div class="container">
+    <div class="dot-container">
+       <div class="dot dot-1"></div>
+       <div class="dot dot-2"></div>
+       <div class="dot dot-3"></div>
+    </div>
+  </div>
+
+</body>
+</html>
